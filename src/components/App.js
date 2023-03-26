efc9cd85906020570b4bb87c9670eb6f69e5cb02
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-26 21:49:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState,useEffect } from "react";
 
 const App = () => {
+  let [input,setInput]=useState('')
+
+  useEffect(() => {
+      console.log(`value changed:${input}`)
+    
+  }, [input]);
+  let handle=(e)=>{
+    setInput(e.target.value)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form>
+          <input type='text' value={input} onChange={handle}/>
+        </form>
     </div>
   )
 }
